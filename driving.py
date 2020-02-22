1a39e9139ed5c1d22bd702154f0e5d043118100d
tunyu <doinb03131047@gmail.com>
2020-02-22 10:55:01 +0800

    firsttt

---
+++ driving.py
@@ -0,0 +1,10 @@
+# 看能不能考駕照
+country = input("請問你是哪國人?")
+age = input("請輸入年齡: ")
+age = int(age)
+
+if country == "台灣":
+    if age >= 18:
+        print("可以考駕照")
+    else:
+        print("還不能考駕照喔")
