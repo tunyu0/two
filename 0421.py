8961ddbc51cfa189851dd6e4605de1555958899f
tunyu0 <61243116+tunyu0@users.noreply.github.com>
2020-04-21 03:19:50 +0800

    second

--- 0421.py
+++ 0421.py
@@ -6,9 +6,9 @@ url = "https://www.books.com.tw/web/sys_bbotm/books/020806/?loc=P_0001_3_006"
 for times in range(3):
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
-    articles = soup.select('.msg h4') #h4 a
+    articles = soup.select('h4 a') 
     paging = soup.select('.nxt')
     next_url = paging[0]['href']
     url = next_url
     for each_title in articles:
-        print(each_title.text)
+        print(each_title.text, each_title['href'])
