7df8b1c5d76b35ff96a72753ae36c1dac03e1d9e
tunyu0 <61243116+tunyu0@users.noreply.github.com>
2020-04-21 03:15:04 +0800

    first

---
+++ 0421.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.books.com.tw/web/sys_bbotm/books/020806/?loc=P_0001_3_006"
+
+for times in range(3):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    articles = soup.select('.msg h4') #h4 a
+    paging = soup.select('.nxt')
+    next_url = paging[0]['href']
+    url = next_url
+    for each_title in articles:
+        print(each_title.text)
