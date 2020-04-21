0a05c599325565040eb1f43f48058ba6ce534c3a
tunyu0 <61243116+tunyu0@users.noreply.github.com>
2020-04-21 22:13:34 +0800

    third

--- 0421.py
+++ 0421.py
@@ -1,14 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-
 url = "https://www.books.com.tw/web/sys_bbotm/books/020806/?loc=P_0001_3_006"
-
-for times in range(3):
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    articles = soup.select('h4 a') 
-    paging = soup.select('.nxt')
-    next_url = paging[0]['href']
+for times in range(5):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    title = soup.select('h4 a')
+    pagging = soup.select('.cnt_page a')
+    price = soup.select('strong+ strong')
+    next_url = pagging[4]['href']
     url = next_url
-    for each_title in articles:
-        print(each_title.text, each_title['href'])
+    for each_title,each_price in zip(title, price):
+        print('書名:',each_title.text, ' 售價:',each_price.get_text())
